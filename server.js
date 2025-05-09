01990a3ff6f23c02b1aa3f7b0e9d29c5eb261fd7
Sneha Gupta <snehagpt1810@gmail.com>
2025-05-09 23:01:44 +0530

    Updated server

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.post("/submit-review", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
